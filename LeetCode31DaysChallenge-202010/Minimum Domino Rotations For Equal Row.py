031efb621bf5a8648fa660c375e732c59adce18c
Haozhi Tan <47723210+HaozhiTan@users.noreply.github.com>
2020-10-20 20:55:13 +1300

    Day 19 Minimum Domino Rotations For Equal Row

--- LeetCode31DaysChallenge-202010/Minimum Domino Rotations For Equal Row.py
+++ LeetCode31DaysChallenge-202010/Minimum Domino Rotations For Equal Row.py
@@ -3,11 +3,12 @@ from typing import List
 
 class Solution:
     def minDominoRotations(self, A: List[int], B: List[int]) -> int:
-        s, n = {1, 2, 3, 4, 5, 6}, len(A)
+        s = {1, 2, 3, 4, 5, 6}
+        n = len(A)
         for i in range(n):
             s &= {A[i], B[i]}
         if not s:
             return -1
-        flips1 = sum(A[i] == list(s)[0] for i in range(n))
-        flips2 = sum(B[i] == list(s)[0] for i in range(n))
-        return min(n - flips1, n - flips2)
+        rotate1 = sum(A[i] != list(s)[0] for i in range(n))
+        rotate2 = sum(B[i] != list(s)[0] for i in range(n))
+        return min(rotate1, rotate2)
